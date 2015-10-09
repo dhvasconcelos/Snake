963402f5ed66f75a8dc33060562809201c06b7f4
dhvasconcelos <diogohvasconcelos@gmail.com>
2015-10-09 09:57:44 +0100

    Different food uploaded

---
+++ Snake/src/org/academia/latehours/objects/NormalFood.java
@@ -0,0 +1,16 @@
+package org.academia.latehours.objects;
+
+
+
+/**
+ * Created by cadet on 06/10/15.
+ */
+
+public class NormalFood extends Food {
+
+    public NormalFood() {
+        super();
+        setFoodScore(10);
+    }
+
+}
---
+++ Snake/src/org/academia/latehours/objects/SpecialFood.java
@@ -0,0 +1,17 @@
+package org.academia.latehours.objects;
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+
+/**
+ * Created by cadet on 08/10/15.
+ */
+public class SpecialFood extends Food{
+
+    public SpecialFood() {
+        super();
+        setFoodScore(25);
+        setColor(Color.RED);
+    }
+
+
+}
